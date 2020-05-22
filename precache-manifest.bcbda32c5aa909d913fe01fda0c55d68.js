ecc99b474d44c39a95101293edb229b984a43f95
pratikagashe <pratikagashe007@gmail.com>
2020-05-22 18:54:59 +0530

    initial build push

---
+++ precache-manifest.bcbda32c5aa909d913fe01fda0c55d68.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9a0068d0b239324a14c682ed33024a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c4156bbbf29c6767864",
+    "url": "/static/css/main.fa436abf.chunk.css"
+  },
+  {
+    "revision": "ac55c0297601dd41a416",
+    "url": "/static/js/2.2db8e520.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2db8e520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4156bbbf29c6767864",
+    "url": "/static/js/main.b05c996d.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "2b4a821eec18a9590d1c7eebb4019905",
+    "url": "/static/media/about.2b4a821e.jpg"
+  },
+  {
+    "revision": "2b4a821eec18a9590d1c7eebb4019905",
+    "url": "/static/media/heroImage.2b4a821e.jpg"
+  }
+]);
